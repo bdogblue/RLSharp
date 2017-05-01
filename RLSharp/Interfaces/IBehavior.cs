fd77069041457b56ea1f843fad729278345b139d
bdog blue <bdog blue>
2017-05-01 17:49:58 -0400

    Add project files.

---
+++ RLSharp/Interfaces/IBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RLSharp.Core;
+using RLSharp.Systems;
+
+namespace RLSharp.Interfaces
+{
+    public interface IBehavior
+    {
+        bool Act(Monster monster, CommandSystem commandSystem);
+    }
+}
---
+++ RLSharp/Interfaces/IDrawable.cs
@@ -0,0 +1,16 @@
+﻿using RLNET;
+using RogueSharp;
+using RLSharp.Systems;
+
+namespace RLSharp.Interfaces
+{
+    public interface IDrawable
+    {
+        RLColor Color { get; set; }
+        char Symbol { get; set; }
+        int X { get; set; }
+        int Y { get; set; }
+
+        void Draw(RLConsole console, IMap map);
+    }
+}
